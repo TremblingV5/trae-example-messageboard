a1e7db8dfcbaa1e916d00fb9f040c41f39ad7294
Xin ZF <43563921+TremblingV5@users.noreply.github.com>
2026-05-31 04:29:13 +0800

    fix: 修复前端QA审查问题 - 创建页面组件+修复Bug+安全增强

---
+++ frontend/src/pages/index.js
@@ -0,0 +1,7 @@
+export { default as Home } from './Home';
+export { default as Login } from './Login';
+export { default as Register } from './Register';
+export { default as PostDetail } from './PostDetail';
+export { default as CreatePost } from './CreatePost';
+export { default as UserProfile } from './UserProfile';
+export { default as Settings } from './Settings';
